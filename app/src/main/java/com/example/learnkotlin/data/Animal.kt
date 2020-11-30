068d3f63a1ac3545a37f60e1865230127e3bdf5b
dailei <dailei@coocaa.com>
2020-11-30 21:58:57 +0800

    类的使用

---
+++ app/src/main/java/com/example/learnkotlin/data/Animal.kt
@@ -0,0 +1,17 @@
+package com.example.learnkotlin.data
+
+/**
+ * description:
+ * Created by dailei on 2020/11/30
+ *
+ */
+open class Animal {
+    open fun eat(){
+        println("Animal eat")
+    }
+
+    open fun jump(){
+        println("Animal jump")
+    }
+
+}
---
+++ app/src/main/java/com/example/learnkotlin/data/Dog.kt
@@ -0,0 +1,22 @@
+package com.example.learnkotlin.data
+
+/**
+ * description:
+ * Created by dailei on 2020/11/30
+ *
+ */
+class Dog(private val name:String):Animal() {
+
+    init {
+        println("$name init")
+    }
+
+    constructor(name:String, age:Int) : this(name) {
+        println("$name , $age init")
+    }
+
+    override fun eat() {
+        super.eat()
+        println("$name dog eat")
+    }
+}
